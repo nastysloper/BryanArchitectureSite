bb991820d11490573070321d59fdefb5f79cfcba
Rich Vogt <richvogtjr@gmail.com>
2019-06-19 09:41:03 -0400

    Sweet new architecture site dude

---
+++ index.js
@@ -0,0 +1,13 @@
+function toggleMenu() {
+	var menuIcon = document.getElementById("menuIcon")
+	var menu = document.getElementById("menu")
+	if (menu.style.display == "block") {
+		menu.style.display = "none"
+	} else {
+		menu.style.display = "block"
+	}
+}
+
+function hideMenu() {
+
+}
